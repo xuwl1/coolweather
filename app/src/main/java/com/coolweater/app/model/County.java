9f471d9dac65b7e41690d29697e286a78a7fd151
xuwl <xuwanglong1@gmail.com>
2016-06-16 17:16:09 +0800

    新增数据库帮助类，以及各表对应的实体类

---
+++ app/src/main/java/com/coolweater/app/model/County.java
@@ -0,0 +1,39 @@
+package com.coolweater.app.model;
+
+/**
+ * Created by Administrator on 2016/6/14.
+ */
+public class County {
+    private int id;
+    private String countyName;
+    private String countyCode;
+    private int cityId;
+
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getCountyName(){
+        return countyName;
+    }
+    public void setCountyName(String countyName){
+        this.countyName = countyName;
+    }
+
+    public String getCountyCode(){
+        return countyCode;
+    }
+    public void setCountyCode(String countyCode){
+        this.countyCode = countyCode;
+    }
+
+    public int getCityId(){
+        return cityId;
+    }
+    public void setCityId(int cityId){
+        this.cityId = cityId;
+    }
+}
